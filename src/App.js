00212fca90195ceb43a74fdf4934fc3ff390c5be
PMSRINIVAS <killermaddy55@gmail.com>
2021-07-02 20:28:12 +0530

    simple react website design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <MySpaTag />
+    </div>
+  );
+}
+
+function MySpaTag() {
+  const [list] = useState([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+  return (
+    <div>
+      <h2 className="bg-dark text-light p-3 sticky-top text-center">
+        Project Book
+      </h2>
+
+      {list.map((item, index) => (
+        <div className="bg-secondary text-light p-3 m-3 rounded">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Voluptatibus, excepturi.
+        </div>
+      ))}
     </div>
   );
 }

20237f18f1e62951bdaea3a00f59fff84e4de717
PMSRINIVAS <killermaddy55@gmail.com>
2021-07-02 21:27:14 +0530

    Multiple page website

--- src/App.js
+++ src/App.js
@@ -5,21 +5,74 @@ import { useState } from "react";
 function App() {
   return (
     <div>
-      <MySpaTag />
+      <MyAppHomePage />
+      <MyAppExplorePage />
+      <MyAppNotificationPage />
+      <MyAppMessagesPage />
     </div>
   );
 }
 
-function MySpaTag() {
+function MyAppHomePage() {
+  const [list] = useState([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+  return (
+    <div>
+      <h2 className="bg-dark text-light p-3 sticky-top">Home</h2>
+
+      {list.map((item, index) => (
+        <div className="bg-secondary text-light p-3 m-3 rounded">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Voluptatibus, excepturi.
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function MyAppExplorePage() {
+  const [list] = useState([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+  return (
+    <div>
+      <h2 className="bg-dark text-light p-3 sticky-top text-center">Explore</h2>
+
+      {list.map((item, index) => (
+        <div className="bg-info text-light p-3 m-3 rounded">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Voluptatibus, excepturi.
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function MyAppNotificationPage() {
   const [list] = useState([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
   return (
     <div>
       <h2 className="bg-dark text-light p-3 sticky-top text-center">
-        Project Book
+        Notification
       </h2>
 
       {list.map((item, index) => (
-        <div className="bg-secondary text-light p-3 m-3 rounded">
+        <div className="bg-success text-light p-3 m-3 rounded">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Voluptatibus, excepturi.
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function MyAppMessagesPage() {
+  const [list] = useState([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+  return (
+    <div>
+      <h2 className="bg-dark text-light p-3 sticky-top text-center">
+        Messages
+      </h2>
+
+      {list.map((item, index) => (
+        <div className="bg-warning text-light p-3 m-3 rounded">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit.
           Voluptatibus, excepturi.
         </div>
